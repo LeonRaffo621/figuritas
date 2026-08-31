44c569a57a0b82c380c765066c0898df1a58cc80
LeonRaffo621 <leonraffoestudio@gmail.com>
2026-08-31 03:36:43 -0300

    edicion de funciones y main

--- Funciones.py
+++ Funciones.py
@@ -1,13 +1,13 @@
 def def_n(n):
-      if str(n).isnumeric():
+    if str(n).isnumeric():
         numero = int(n)
         return (numero)
-      else:
+    else:
         return False
 
 def convertidor_Fecha(F):
     n=def_n(F)
-    if n :
+    if n:
         dia =n // 1000000
         mes = (n// 10000) % 100
         año = n % 10000 
@@ -16,13 +16,13 @@ def convertidor_Fecha(F):
 def Es_bisiesto(F):
     convertido = convertidor_Fecha(F)[2]
     if convertido % 400 == 0:
-            return 29
+        return 29
     elif convertido % 100 == 0:
-            return 28
+        return 28
     elif convertido % 4 == 0:
-            return 29
+        return 29
     else:
-            return 28
+        return 28
     
 def validar_fecha(F):
     if def_n(F):
@@ -39,91 +39,92 @@ def validar_fecha(F):
                         return False
                 elif mes in meses_30:
                     if 1 <= dia <= 30:
-                            return True
+                        return True
                     else:
-                            return False
+                        return False
                 elif mes==2:
                     if 1 <= dia <= Es_bisiesto(F) :
-                            return True
+                        return True
                     else:
-                            return False
+                        return False
             else:
-                  return "Error: Verifique que la fecha sea valida"
+                return "Error: Verifique que la fecha sea valida"
         else:
-              return "Error: Sin dato de fecha"
+            return "Error: Sin dato de fecha"
     else:
-        return "Error: Formato inválido"
+        return "Error: Formato de fecha inválido"
 
 def validar_hora(H):
-     try:
+    try:
         hora = int(H)
         if  hora < 0 or hora > 23:
-                return "Error: Hora fuera de rango"
+            return "Error: Hora fuera de rango"
         else:
-             return True
+            return True
                 
-     except ValueError:
-          return "Error: Formato inválido"
+    except ValueError:
+        return "Error: Formato de hora inválido"
         
 def validar_temperatura(T):
-       try:
-         temp=float(T)
-         return True
-       except ValueError:
-         return "Error: Formato inválido"
+    try:
+        float(T)
+        return True
+    except ValueError:
+        return "Error: Formato de temperatura inválido"
 
 def validar_humedad(HUM):
-       try:
-            H=float(HUM)
-            if  0 <= H and H <= 100:
-                return True
-            else:
-                 return "Error: Humedad invalida"
-       except ValueError:
-             return "Error: Formato inválido"
+    try:
+        H=float(HUM)
+        if  0 <= H and H <= 100:
+            return True
+        else:
+            return "Error: Humedad fuera del rango"
+    except ValueError:
+        return "Error: Formato de la humedad inválido"
 
 def validar_presion_viento(P):
-      
-      if P == "":
-           return True
-      try:    
-          hpa=float(P)
-          if 0 <= hpa:
-              return True
-          else:
-             return "Error: Numero no valido"   
-      except ValueError:
-           return "Error: Formato inválido"
+    if P == "":
+        return True
+    try:
+        hpa=float(P)
+        if 0 <= hpa:
+            return True
+        else:
+            return "Error: fuera de rango"   
+    except ValueError:
+        return "Error: Formato inválido"
       
 def validar_direccion(d):
     try:
         num=float(d)
         if 0 <= num <= 360:
-             return True
+            return True
         else:
-             return "Error: Dirección fuera de rango"
+            return "Error: Dirección fuera de rango"
     except ValueError:
-        return "Error: Formato inválido"
+        return "Error: Formato de diraccion inválido"
 
 def reconstruir_registro(AR):
     registros = []
     registro_actual = None
-
-    with open(AR, "r") as archivo:
-        for linea in archivo:
-            linea = linea.rstrip("\n")
-            if linea.strip() == "":
-                continue
-            if linea[:8].strip().isnumeric():
-                if registro_actual is not None:
-                    registros.append(registro_actual)
-                registro_actual = linea
-            else:
-                if registro_actual is not None:
-                    registro_actual = registro_actual.rstrip() + linea[44:].strip()
-        if registro_actual is not None:
-            registros.append(registro_actual)
-    return registros
+    try:
+        with open(AR, "r") as archivo:
+            for linea in archivo:
+                linea = linea.rstrip("\n")
+                if linea.strip() == "":
+                    continue
+                if linea[:8].strip().isnumeric():
+                    if registro_actual is not None:
+                        registros.append(registro_actual)
+                    registro_actual = linea
+                else:
+                    if registro_actual is not None:
+                        registro_actual = registro_actual.rstrip() + linea[44:].strip()
+            if registro_actual is not None:
+                registros.append(registro_actual)
+        return registros
+    except:
+        return("Sin archivo TXT")
 
 def parsear_registros(registro):
     fecha=registro[0:8].strip()
@@ -140,33 +141,34 @@ def validar_registro(datos):
     errores = []
     fecha, hora, temperatura, humedad, presion, direccion, velocidad, nombre = datos
     resultado = validar_fecha(fecha)
-    if resultado != True:
+    if resultado is not True:
         errores.append(resultado)
 
     resultado = validar_hora(hora)
-    if resultado != True:
+    if resultado is not True:
         errores.append(resultado)
 
     resultado =validar_temperatura(temperatura) 
-    if resultado != True:
+    if resultado is not True:
         errores.append(resultado)
 
     resultado = validar_humedad(humedad)   
-    if resultado != True:
+    if resultado is not True:
         errores.append(resultado)
 
     resultado = validar_presion_viento(presion)
-    if resultado != True:
+    if resultado is not True:
         errores.append(resultado)
 
     resultado = validar_direccion(direccion)
-    if resultado != True:
+    if resultado is not True:
         errores.append(resultado)
 
     resultado = validar_presion_viento(velocidad)  
-    if resultado != True:
+    if resultado is not True:
         errores.append(resultado)
     if nombre == "":
         errores.append("Nombre de estación vacío")
 
     return errores
+
--- Main.py
+++ Main.py
@@ -1,22 +1,50 @@
-"""lectura txt"""
+import json
 from Funciones import validar_registro, parsear_registros, reconstruir_registro
-#### PONER ARCHIVO A LEER EN "nombre_archivo" ejemplo "datohorario20260826" ####
+#### PONER EL NOMBRE DE LOS ARCHIVOS EN SUS LUGARES CORRESONDIENTES: 
+# Archivo_txt("nombre del archivo.txt)
+# Archivo.json("nombre del archivo.json") ####
 
-registros = reconstruir_registro("nombre_archivo")
-registros_validos = []
+Archivo_txt=("lecturas.txt")
+Archivo_json=()
+registros = reconstruir_registro(Archivo_txt)
+resumen={}
+registros_validos =[]
 registros_invalidos = []
+clave_val= "Cantidad de registros validos"
+clave_inv="Cantidad de registros invalidos"
 
-for registro in registros:
-    datos = parsear_registros(registro)
-    errores = validar_registro(datos)
-    if len(errores) == 0:
-        registros_validos.append(registro)
-    else:
-        registros_invalidos.append({
-            "linea":registro,
-            "errores":errores
-            })
+try:
+    for registro in registros:
+        datos = parsear_registros(registro)
+        errores = validar_registro(datos)
+        if len(errores) == 0:
+            registros_validos.append(registro)
+            if clave_val not in resumen:
+                resumen[clave_val]=1
+            else:
+                resumen[clave_val]+=1       
+        else:
+            registros_invalidos.append({
+                "linea":registro,
+                "errores":errores
+                })
+            if clave_inv not in resumen:
+                resumen[clave_inv]=1
+            else:
+                resumen[clave_inv]+=1
+    suma_de_registros=resumen[clave_val]+resumen[clave_inv]
+    resumen["cantidad de registros"]=suma_de_registros        
+except:
+    print("Error: No hay archivo TXT para leer")
 
+try:
+    with open (Archivo_json,"w") as Arj:
+        json.dump(resumen,Arj,indent=1)
+        json.dump(registros_validos,Arj,indent=1,ensure_ascii=False)
+        if len(registros_invalidos)>0:
+            json.dump(registros_invalidos,Arj,indent=1,ensure_ascii=False)
+except:
+    print ("Error: No hay archivo JSON para escribir")
 ###  Prueba para ver registros validos ###
 #for registro in registros_validos:
     #print("----Registros valido----", "\n", registro)
